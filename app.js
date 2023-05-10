76b303feda83e0c69a6a623e8f0166403150d7ab
Franco Guagenti <franco.guagenti.94@gmail.com>
2023-05-10 18:48:56 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,75 @@
+let enero = "";
+let febrero = "";
+let marzo = "";
+let abril = "";
+let mayo = "";
+let junio = "";
+let julio = "";
+let agosto = "";
+let septiembre = "";
+let octubre = "";
+let noviembre = "";
+let diciembre = "";
+let resultado;
+let mes = 1;
+
+function promedio() {
+  resultado =
+    parseInt(
+      enero +
+        febrero +
+        marzo +
+        abril +
+        mayo +
+        junio +
+        julio +
+        agosto +
+        septiembre +
+        octubre +
+        noviembre +
+        diciembre
+    ) / 12;
+  alert(`El promedio anual del nivel del mar fue de ${resultado} mts`);
+}
+
+function ingresandoValores(mes) {
+  for (mes >= 1; mes <= 12; mes++) {
+    if (mes == 1) {
+      enero = parseInt(prompt("Ingrese nivel Enero"));
+    } else if (mes == 2) {
+      febrero = parseInt(prompt("Ingrese nivel Febrero"));
+    } else if (mes == 3) {
+      marzo = parseInt(prompt("Ingrese nivel Marzo"));
+    } else if (mes == 4) {
+      abril = parseInt(prompt("Ingrese nivel Abril"));
+    } else if (mes == 5) {
+      mayo = parseInt(prompt("Ingrese nivel Mayo"));
+    } else if (mes == 6) {
+      junio = parseInt(prompt("Ingrese nivel Junio"));
+    } else if (mes == 7) {
+      julio = parseInt(prompt("Ingrese nivel Julio"));
+    } else if (mes == 8) {
+      agosto = parseInt(prompt("Ingrese nivel Agosto"));
+    } else if (mes == 9) {
+      septiembre = parseInt(prompt("Ingrese nivel Septiembre"));
+    } else if (mes == 10) {
+      octubre = parseInt(prompt("Ingrese nivel Octubre"));
+    } else if (mes == 11) {
+      noviembre = parseInt(prompt("Ingrese nivel Noviembre"));
+    } else if (mes == 12) {
+      diciembre = parseInt(prompt("Ingrese nivel Diciembre"));
+    } else {
+      alert("Mes incorrecto");
+    }
+  }
+}
+
+function reiniciarPag() {
+  location.reload();
+}
+
+let botonReiniciar = document.getElementById("boton-reiniciar");
+botonReiniciar.addEventListener("click", reiniciarPag);
+
+ingresandoValores(mes);
+promedio();
